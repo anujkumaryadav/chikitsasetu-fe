ea5f0dd53db21f3664fd80ccc27fa6856911488c
Anujkumar Yadav <anujkumaryadav.in@gmail.com>
2026-01-18 19:13:07 +0530

    Update page.tsx

--- components/LandingPage/page.tsx
+++ components/LandingPage/page.tsx
@@ -175,7 +175,7 @@ export const LandingPage = () => {
                   className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                   disabled
                 />
-                <button className="bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition-colors">
+                <button className="bg-green-600 text-white p-2 rounded-lg hover:bg-green-700 transition-colors">
                   <RiTelegram2Fill />
                 </button>
               </div>
